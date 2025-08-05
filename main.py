a07ddb507aff0a496bdc3e0140545273bf2b4e4e
UltraQbik <no1skill@yandex.ru>
2025-08-05 21:29:42 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Main file
+"""
+
+
+class Application:
+    """
+    Application class
+    """
+
+    def __init__(self):
+        ...
+
+    def run(self):
+        ...
+
+
+def main():
+    app = Application()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
